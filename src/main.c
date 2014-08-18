4a4527a6f608bbb96ba11d1ff1e8b806650146ca
Nicolas Koch <nioko1337@googlemail.com>
2014-08-18 15:28:41 +0200

    cleanups

--- src/main.c
+++ src/main.c
@@ -7,10 +7,10 @@
 void show_help()
 {
 	const char* help_text="Brainfuck interpreter by Nicolas Koch\n"
-					 "Syntax: brainfuck-interpreter [-vh] bf-filename\n"
-					 "Options:\n"
-					 "\t-h: show this help\n"
-					 "\t-v: show version information\n";
+					      "Syntax: brainfuck-interpreter [-vh] bf-filename\n"
+					      "Options:\n"
+					      "\t-h: show this help\n"
+					      "\t-v: show version information\n";
     printf("%s", help_text);
 }
 
