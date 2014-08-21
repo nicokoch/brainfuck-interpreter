3a8f145a887c60d0f1772b7e90c9eacb053f7fdc
Nicolas Koch <nioko1337@googlemail.com>
2014-08-21 10:29:22 +0200

    coding style fixes

--- src/brainfuck.c
+++ src/brainfuck.c
@@ -8,188 +8,202 @@
 /*
  * Pretty print the current brainfuck array
  */
-void print_current_state(CELL *prog_array, CELL* ptr, int arr_size)
+void print_current_state(CELL * prog_array, CELL * ptr, int arr_size)
 {
-    int ptr_index = ptr - prog_array;
-    int start_i = ptr_index - ERR_HANDLE_STACK_THRESH < 0 ? 0 : ptr_index - ERR_HANDLE_STACK_THRESH;
-    int end_i = ptr_index + ERR_HANDLE_STACK_THRESH < arr_size ? ptr_index + ERR_HANDLE_STACK_THRESH : arr_size - 1;
-    int i;
+	int ptr_index = ptr - prog_array;
+	int start_i =
+	    ptr_index - ERR_HANDLE_STACK_THRESH <
+	    0 ? 0 : ptr_index - ERR_HANDLE_STACK_THRESH;
+	int end_i =
+	    ptr_index + ERR_HANDLE_STACK_THRESH <
+	    arr_size ? ptr_index + ERR_HANDLE_STACK_THRESH : arr_size - 1;
+	int i;
 
-    fprintf(stderr, "Indices:");
-    for(i=start_i; i<end_i; i++){
-        fprintf(stderr, " %5d ", i);
-    }
-    fprintf(stderr, "\n");
+	fprintf(stderr, "Indices:");
+	for (i = start_i; i < end_i; i++) {
+		fprintf(stderr, " %5d ", i);
+	}
+	fprintf(stderr, "\n");
 
-    fprintf(stderr, "Array  :");
-    for(i=start_i; i<end_i; i++){
-        fprintf(stderr, " %5d ", prog_array[i]);
-    }
-    fprintf(stderr, "\n");
+	fprintf(stderr, "Array  :");
+	for (i = start_i; i < end_i; i++) {
+		fprintf(stderr, " %5d ", prog_array[i]);
+	}
+	fprintf(stderr, "\n");
 
-    fprintf(stderr, "Pointer:");
-    char ptr_symbol = '^';
-    for(i=start_i; i<end_i; i++){
-        if(i == ptr_index){
-            fprintf(stderr, " %5c ", ptr_symbol);
-        }
-        else{
-            fprintf(stderr, "       ");
-        }
-    }
-    fprintf(stderr, "\n");
+	fprintf(stderr, "Pointer:");
+	char ptr_symbol = '^';
+	for (i = start_i; i < end_i; i++) {
+		if (i == ptr_index) {
+			fprintf(stderr, " %5c ", ptr_symbol);
+		} else {
+			fprintf(stderr, "       ");
+		}
+	}
+	fprintf(stderr, "\n");
 }
 
 /*
  * Pretty print the code position of an error
  */
-void print_code_position(char* prog_code, int code_pos){
-    int code_len = strlen(prog_code);
+void print_code_position(char *prog_code, int code_pos)
+{
+	int code_len = strlen(prog_code);
 
-    //get line number and line
-    int line_cnt = 1;
-    int line_start=0;
-    int i;
-    for(i=0; i<code_pos; i++){
-        if(prog_code[i] == '\n'){
-            line_cnt++;
-            line_start = i+1;
-        }
-    }
-    fprintf(stderr, "In line %d:\n", line_cnt);
-    i = line_start;
-    char cur = prog_code[i];
-    while(cur != '\n' && i<code_len){
-        fprintf(stderr, "%c", cur);
+	//get line number and line
+	int line_cnt = 1;
+	int line_start = 0;
+	int i;
+	for (i = 0; i < code_pos; i++) {
+		if (prog_code[i] == '\n') {
+			line_cnt++;
+			line_start = i + 1;
+		}
+	}
+	fprintf(stderr, "In line %d:\n", line_cnt);
+	i = line_start;
+	char cur = prog_code[i];
+	while (cur != '\n' && i < code_len) {
+		fprintf(stderr, "%c", cur);
 
-        i++;
-        cur = prog_code[i];
-    }
-    fprintf(stderr, "\n");
-    int pos_in_line = code_pos - (line_start);
-    i = 0;
-    while(i < pos_in_line){
-        fprintf(stderr, " ");
-        i++;
-    }
-    fprintf(stderr, "^\n");
+		i++;
+		cur = prog_code[i];
+	}
+	fprintf(stderr, "\n");
+	int pos_in_line = code_pos - (line_start);
+	i = 0;
+	while (i < pos_in_line) {
+		fprintf(stderr, " ");
+		i++;
+	}
+	fprintf(stderr, "^\n");
 }
 
 /*
  * High level function to deal with brainfuck programming errors
  */
-void deal_with_error(char* prog_code, int code_index, CELL* prog_array, CELL* ptr)
+void deal_with_error(char *prog_code, int code_index, CELL * prog_array,
+		     CELL * ptr)
 {
-    fprintf(stderr, "Current BRAINFUCK stack:\n");
-    print_current_state(prog_array, ptr, DEFAULT_ARRAY_SIZE);
-    print_code_position(prog_code, code_index);
+	fprintf(stderr, "Current BRAINFUCK stack:\n");
+	print_current_state(prog_array, ptr, DEFAULT_ARRAY_SIZE);
+	print_code_position(prog_code, code_index);
 }
 
 /*
  * O(n) function to get matching brackets -> TODO: replace this with a stack data structure
  */
-int get_matching_close_index(char* string, int index)
+int get_matching_close_index(char *string, int index)
 {
-    if(string[index] != '['){
-        fprintf(stderr, "Interpreter error: char at position %d does not equal '['\n", index);
-        return -ERROR_INTERN;
-    }
-    unsigned int i = index;
-    int opencnt = 0;
-    while(i < strlen(string)){
-        char cur = string[i];
-        if (cur == '['){
-            opencnt++;
-        }
-        else if(cur == ']'){
-            opencnt--;
-        }
-        if(opencnt == 0){
-            return i;
-        }
-        i++;
-    }
-    fprintf(stderr, "Syntax error: no matching close bracket found for char at position %d\n", index);
-    return -ERROR_SYNTAX;
+	if (string[index] != '[') {
+		fprintf(stderr,
+			"Interpreter error: char at position %d does not equal '['\n",
+			index);
+		return -ERROR_INTERN;
+	}
+	unsigned int i = index;
+	int opencnt = 0;
+	while (i < strlen(string)) {
+		char cur = string[i];
+		if (cur == '[') {
+			opencnt++;
+		} else if (cur == ']') {
+			opencnt--;
+		}
+		if (opencnt == 0) {
+			return i;
+		}
+		i++;
+	}
+	fprintf(stderr,
+		"Syntax error: no matching close bracket found for char at position %d\n",
+		index);
+	return -ERROR_SYNTAX;
 }
 
-int get_matching_open_index(char* string, int index)
+int get_matching_open_index(char *string, int index)
 {
-    if(string[index] != ']'){
-        fprintf(stderr, "Interpreter error: char at position %d does not equal ']'\n", index);
-        return -ERROR_INTERN;
-    }
-    unsigned int i = index;
-    int closecnt = 0;
-    while(i != 0){
-        char cur = string[i];
-        if (cur == ']'){
-            closecnt++;
-        }
-        else if(cur == '['){
-            closecnt--;
-        }
-        if(closecnt == 0){
-            return i;
-        }
-        i--;
-    }
-    fprintf(stderr, "Syntax error: no matching open bracket found for char at position %d\n", index);
-    return -ERROR_SYNTAX;
+	if (string[index] != ']') {
+		fprintf(stderr,
+			"Interpreter error: char at position %d does not equal ']'\n",
+			index);
+		return -ERROR_INTERN;
+	}
+	unsigned int i = index;
+	int closecnt = 0;
+	while (i != 0) {
+		char cur = string[i];
+		if (cur == ']') {
+			closecnt++;
+		} else if (cur == '[') {
+			closecnt--;
+		}
+		if (closecnt == 0) {
+			return i;
+		}
+		i--;
+	}
+	fprintf(stderr,
+		"Syntax error: no matching open bracket found for char at position %d\n",
+		index);
+	return -ERROR_SYNTAX;
 }
 
 /*
  * Read a file into heap string
  */
-char* bf_read_file(char* filename)
+char *bf_read_file(char *filename)
 {
-    char* buffer = 0;
-    long length;
-    FILE* f = fopen (filename, "rb");
+	char *buffer = 0;
+	long length;
+	FILE *f = fopen(filename, "rb");
 
-    if (f){
-      fseek (f, 0, SEEK_END);
-      length = ftell (f);
-      fseek (f, 0, SEEK_SET);
-      buffer = malloc(length);
-      if (buffer)
-      {
-        int read_bytes = fread(buffer, 1, length, f);
-        if(read_bytes != length){
-			fprintf(stderr, "Error while reading file: %s\n", filename);
-			return NULL;
+	if (f) {
+		fseek(f, 0, SEEK_END);
+		length = ftell(f);
+		fseek(f, 0, SEEK_SET);
+		buffer = malloc(length);
+		if (buffer) {
+			int read_bytes = fread(buffer, 1, length, f);
+			if (read_bytes != length) {
+				fprintf(stderr,
+					"Error while reading file: %s\n",
+					filename);
+				return NULL;
+			}
 		}
-      }
-      fclose (f);
-      return buffer;
-    } else {
-        fprintf(stderr, "File not found: %s\n", filename);
-        return NULL;
-    }
+		fclose(f);
+		return buffer;
+	} else {
+		fprintf(stderr, "File not found: %s\n", filename);
+		return NULL;
+	}
 }
 
 /*
  * Read the file, then remove non-brainfuck characters, effectively minimizing the file
  */
-char* bf_read_file_and_minimize(char* filename)
+char *bf_read_file_and_minimize(char *filename)
 {
-    char* file_contents = bf_read_file(filename);
-    if(!file_contents) return NULL;
-    file_contents = bf_minimize_file(file_contents);
-    return file_contents;
+	char *file_contents = bf_read_file(filename);
+	if (!file_contents)
+		return NULL;
+	file_contents = bf_minimize_file(file_contents);
+	return file_contents;
 }
 
 /*
  * Create an empty program array
  */
-CELL* bf_create_array(int arr_size)
+CELL *bf_create_array(int arr_size)
 {
-	CELL* result = malloc(arr_size * sizeof(CELL));
-	memset(result, '\0', arr_size); //suck it, calloc
+	CELL *result = malloc(arr_size * sizeof(CELL));
+	memset(result, '\0', arr_size);	//suck it, calloc
 	return result;
 }
 
-void bf_destroy_array(CELL* prog_array){
+void bf_destroy_array(CELL * prog_array)
+{
 	free(prog_array);
 }
 
@@ -205,48 +219,48 @@ void bf_destroy_array(CELL* prog_array){
  * 
  * @returns: The resulting instruction pointer
  */
-CELL* bf_execute(char* program, CELL* prog_array, CELL* ptr)
+CELL *bf_execute(char *program, CELL * prog_array, CELL * ptr)
 {
-    int i=0;
-    int prev_i;
-    while(i < strlen(program)){
-        prev_i=i;
-        char token = program[i];
-        switch (token){
-            case '<':
-                ptr--;
-                break;
-            case '>':
-                ptr++;
-                break;
-            case '+':
-                ++*ptr;
-                break;
-            case '-':
-                --*ptr;
-                break;
-            case '.':
-                putchar(*ptr);
-                break;
-            case ',':
-                *ptr = getchar();
-                break;
-            case '[':
-                if(*ptr == 0){
-                    i = get_matching_close_index(program, i);
-                }
-                break;
-            case ']':
-                if (*ptr != 0){
-                    i = get_matching_open_index(program, i);
-                }
-                break;
-        }
-        if(i < 0){
-            deal_with_error(program,prev_i,prog_array, ptr);
-            return NULL;
-        }
-        i++;
-    }
-    return ptr;
+	int i = 0;
+	int prev_i;
+	while (i < strlen(program)) {
+		prev_i = i;
+		char token = program[i];
+		switch (token) {
+		case '<':
+			ptr--;
+			break;
+		case '>':
+			ptr++;
+			break;
+		case '+':
+			++*ptr;
+			break;
+		case '-':
+			--*ptr;
+			break;
+		case '.':
+			putchar(*ptr);
+			break;
+		case ',':
+			*ptr = getchar();
+			break;
+		case '[':
+			if (*ptr == 0) {
+				i = get_matching_close_index(program, i);
+			}
+			break;
+		case ']':
+			if (*ptr != 0) {
+				i = get_matching_open_index(program, i);
+			}
+			break;
+		}
+		if (i < 0) {
+			deal_with_error(program, prev_i, prog_array, ptr);
+			return NULL;
+		}
+		i++;
+	}
+	return ptr;
 }
--- src/shell.c
+++ src/shell.c
@@ -9,14 +9,14 @@
 
 void show_shell_help()
 {
-    const char* help =  "q: quit\n"
-                        "help: show this help\n\n"
-                        "Brainfuck cheatsheet:\n"
-                        "\t< >: move the pointer\n"
-                        "\t+ -: increment/decrement the value at pointer position\n"
-                        "\t. ,: write/read text to/from stdout/stdin\n"
-                        "\t[ ]: start/end loop";
-    printf("%s\n", help);
+	const char *help = "q: quit\n"
+	    "help: show this help\n\n"
+	    "Brainfuck cheatsheet:\n"
+	    "\t< >: move the pointer\n"
+	    "\t+ -: increment/decrement the value at pointer position\n"
+	    "\t. ,: write/read text to/from stdout/stdin\n"
+	    "\t[ ]: start/end loop";
+	printf("%s\n", help);
 }
 
 /*
@@ -24,23 +24,22 @@ void show_shell_help()
  */
 int run_brainfuck_shell()
 {
-    CELL* prog_array = bf_create_array(DEFAULT_ARRAY_SIZE);
-    CELL* ptr = prog_array;
-    char buffer[SHELL_BUFFER_LENGTH];
-    while(buffer[0] != SHELL_QUIT_CHAR){
-        memset(buffer, '\0', SHELL_BUFFER_LENGTH);
-        printf(">>> ");
-        fgets(buffer, SHELL_BUFFER_LENGTH, stdin);
-        if(strcmp(buffer, SHELL_HELP) == 0){
-            show_shell_help();
-        }
-        else{
-            ptr = bf_execute(buffer, prog_array, ptr);
-            if(!ptr){
-                ptr = prog_array;
-            }
-            //print_current_state(prog_array, ptr, DEFAULT_ARRAY_SIZE);
-        }
-    }
-    return 0;
+	CELL *prog_array = bf_create_array(DEFAULT_ARRAY_SIZE);
+	CELL *ptr = prog_array;
+	char buffer[SHELL_BUFFER_LENGTH];
+	while (buffer[0] != SHELL_QUIT_CHAR) {
+		memset(buffer, '\0', SHELL_BUFFER_LENGTH);
+		printf(">>> ");
+		fgets(buffer, SHELL_BUFFER_LENGTH, stdin);
+		if (strcmp(buffer, SHELL_HELP) == 0) {
+			show_shell_help();
+		} else {
+			ptr = bf_execute(buffer, prog_array, ptr);
+			if (!ptr) {
+				ptr = prog_array;
+			}
+			//print_current_state(prog_array, ptr, DEFAULT_ARRAY_SIZE);
+		}
+	}
+	return 0;
 }
