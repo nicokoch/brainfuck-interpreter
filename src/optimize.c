af8f1f67d87226e761373236671269e784ac6921
Nicolas Koch <nioko1337@googlemail.com>
2014-08-27 00:06:58 +0000

    prettify code

--- src/optimize.c
+++ src/optimize.c
@@ -6,23 +6,26 @@
  * Remove all non-brainfuck characters from the given program string.
  * Alters the current file_string -> very efficient
  */
-char *bf_minimize_file(char *file_string)
+char *
+bf_minimize_file (char *file_string)
 {
-	const char *valid_chars = "<>.,+-[]";
-	int i;
-	int insert_pos = 0;
-	int code_length = strlen(file_string);
-	for (i = 0; i < code_length; i++) {
-		char current_char = file_string[i];
-		if (strchr(valid_chars, current_char)) {
-			//character is valid
-			file_string[insert_pos] = current_char;
-			insert_pos++;
-		}
+  const char *valid_chars = "<>.,+-[]";
+  int i;
+  int insert_pos = 0;
+  int code_length = strlen (file_string);
+  for (i = 0; i < code_length; i++)
+    {
+      char current_char = file_string[i];
+      if (strchr (valid_chars, current_char))
+	{
+	  //character is valid
+	  file_string[insert_pos] = current_char;
+	  insert_pos++;
 	}
-	//add trailing null-byte
-	file_string[insert_pos] = '\0';
-	//reduce heap memory
-	file_string = realloc(file_string, insert_pos + 1);
-	return file_string;
+    }
+  //add trailing null-byte
+  file_string[insert_pos] = '\0';
+  //reduce heap memory
+  file_string = realloc (file_string, insert_pos + 1);
+  return file_string;
 }
